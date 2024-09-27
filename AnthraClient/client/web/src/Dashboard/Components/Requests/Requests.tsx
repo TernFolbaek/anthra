c9911cb482c2e92364ea440ef163de3a1e186735
Tern Yuka Følbæk <tfolbaek@gmail.com>
2024-09-27 06:40:20 +0200

    Add Requests component

---
+++ AnthraClient/client/web/src/Dashboard/Components/Requests/Requests.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Requests: React.FC = () => {
+    return (
+        <div>
+            <h1>Requests</h1>
+            {/* Content goes here */}
+        </div>
+    );
+};
+
+export default Requests;

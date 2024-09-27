ffb0ad0a6f6d9e435b147921b835f43413d0ecae
Tern Yuka Følbæk <tfolbaek@gmail.com>
2024-09-27 06:40:02 +0200

    Add Messages component

---
+++ AnthraClient/client/web/src/Dashboard/Components/Messages/Messages.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Messages: React.FC = () => {
+    return (
+        <div>
+            <h1>Messages</h1>
+            {/* Content goes here */}
+        </div>
+    );
+};
+
+export default Messages;

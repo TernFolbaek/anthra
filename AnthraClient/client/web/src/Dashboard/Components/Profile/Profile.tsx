632da4e980e1d4d252622818027aab374e8d7ae0
Tern Yuka Følbæk <tfolbaek@gmail.com>
2024-09-27 06:40:13 +0200

    Add Profile component

---
+++ AnthraClient/client/web/src/Dashboard/Components/Profile/Profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile: React.FC = () => {
+    return (
+        <div>
+            <h1>Your Profile</h1>
+            {/* Content goes here */}
+        </div>
+    );
+};
+
+export default Profile;

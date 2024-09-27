f043b45c9d178571873af5b5bbfd49b7b50f5633
Tern Yuka Følbæk <tfolbaek@gmail.com>
2024-09-27 06:38:21 +0200

    Add ExplorePage component

---
+++ AnthraClient/client/web/src/Dashboard/Components/ExplorePage/ExplorePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ExplorePage: React.FC = () => {
+    return (
+        <div>
+            <h1>Explore Page</h1>
+            {/* Content goes here */}
+        </div>
+    );
+};
+
+export default ExplorePage;

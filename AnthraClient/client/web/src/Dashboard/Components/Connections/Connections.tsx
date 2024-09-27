d994c5b1ed9de8b2282cd555dce95b9d6f052daa
Tern Yuka Følbæk <tfolbaek@gmail.com>
2024-09-27 06:37:25 +0200

    Add Connections component

---
+++ AnthraClient/client/web/src/Dashboard/Components/Connections/Connections.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Connections: React.FC = () => {
+    return (
+        <div>
+            <h1>Connections</h1>
+            {/* Content goes here */}
+        </div>
+    );
+};
+
+export default Connections;

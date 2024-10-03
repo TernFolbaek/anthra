93f0954c6740be68a9510c077b92af77b08fb10e
Tern Yuka Følbæk <tfolbaek@gmail.com>
2024-10-03 18:08:09 +0200

    Create blob storage for saving profile images

--- AnthraServer/Controllers/ProfileController.cs
+++ AnthraServer/Controllers/ProfileController.cs
@@ -1,5 +1,4 @@
 // Controllers/ProfileController.cs
-
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -8,16 +7,20 @@ using MyBackendApp.Models;
 using MyBackendApp.ViewModels;
 using System.Threading.Tasks;
 using System.IO;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 
 namespace MyBackendApp.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    [Authorize]
+
     public class ProfileController : ControllerBase
     {
         private readonly UserManager<ApplicationUser> _userManager;
-
+        
+        private readonly string _storageConnectionString = "BlobEndpoint=https://anthra.blob.core.windows.net/;QueueEndpoint=https://anthra.queue.core.windows.net/;FileEndpoint=https://anthra.file.core.windows.net/;TableEndpoint=https://anthra.table.core.windows.net/;SharedAccessSignature=sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2026-10-01T16:15:49Z&st=2024-10-01T08:15:49Z&spr=https&sig=SHmifWmLLf50pO0nqEVnIBYTqRx0QHmJpS5iAiYXq%2F0%3D";
+        private readonly string _containerName = "profile-pictures";
         public ProfileController(UserManager<ApplicationUser> userManager)
         {
             _userManager = userManager;
@@ -71,8 +74,15 @@ namespace MyBackendApp.Controllers
             user.AboutMe = model.AboutMe;
             user.Age = model.Age;
             user.CreatedProfile = true;
+            
 
             var result = await _userManager.UpdateAsync(user);
+            if (model.ProfilePicture != null && model.ProfilePicture.Length > 0)
+            {
+                // Upload to Azure Blob Storage
+                var blobUrl = await UploadProfilePictureToAzure(userId, model.ProfilePicture);
+                user.ProfilePictureUrl = blobUrl;
+            }
 
             if (result.Succeeded)
             {
@@ -83,6 +93,32 @@ namespace MyBackendApp.Controllers
             return BadRequest(new { Errors = updateErrors });
         }
 
+        private async Task<string> UploadProfilePictureToAzure(string userId, IFormFile profilePicture)
+        {
+            // Create a BlobServiceClient
+            BlobServiceClient blobServiceClient = new BlobServiceClient(_storageConnectionString);
+
+            // Get the container client
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(_containerName);
+
+            // Create the container if it does not exist
+            await containerClient.CreateIfNotExistsAsync(PublicAccessType.Blob);
+
+            // Set the blob name to the userId
+            string blobName = userId;
+
+            // Get the blob client
+            BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+            // Upload the image
+            await using (var stream = profilePicture.OpenReadStream())
+            {
+                await blobClient.UploadAsync(stream, true);
+            }
+
+            // Return the URL to the uploaded image
+            return blobClient.Uri.ToString();
+        }
 
         [HttpGet("GetProfile")]
         public async Task<IActionResult> GetProfile()
@@ -110,7 +146,7 @@ namespace MyBackendApp.Controllers
                 user.Subjects,
                 user.AboutMe,
                 user.Age,
-                ProfilePictureUrl = user.ProfilePictureUrl, // Use the correct property
+                user.ProfilePictureUrl, // Use the correct property
                 user.CreatedProfile
             };
 

cca4714d44e768faaa91d4ca391fd4a19a0df1e1
Tern Yuka Følbæk <tfolbaek@gmail.com>
2024-10-05 10:53:31 +0200

    Create message view model to ensure type safety when it reaches the controller

---
+++ AnthraServer/ViewModels/MessageViewModel.cs
@@ -0,0 +1,18 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace MyBackendApp.ViewModels
+{
+    public class SendMessageModel
+    {
+        [Required]
+        public string SenderId { get; set; }
+        [Required]
+        public string ReceiverId { get; set; }
+        [Required]
+        public string Content { get; set; }
+        
+    }
+}

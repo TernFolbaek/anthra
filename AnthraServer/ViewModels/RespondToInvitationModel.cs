c2d7dae54a77ddedc962e1196c9ebec21147e936
Tern Yuka Følbæk <tfolbaek@gmail.com>
2024-10-06 17:10:27 +0200

    Add response to group invitation model

---
+++ AnthraServer/ViewModels/RespondToInvitationModel.cs
@@ -0,0 +1,9 @@
+// ViewModels/RespondToInvitationModel.cs
+namespace MyBackendApp.ViewModels
+{
+    public class RespondToInvitationModel
+    {
+        public int GroupId { get; set; }
+        public bool Accept { get; set; }
+    }
+}

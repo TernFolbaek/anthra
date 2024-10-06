5894cecec784ae5c026f2f83a8618d1138c67785
Tern Yuka Følbæk <tfolbaek@gmail.com>
2024-10-06 17:12:01 +0200

    Add create group model

---
+++ AnthraServer/ViewModels/CreateGroupModel.cs
@@ -0,0 +1,15 @@
+// ViewModels/CreateGroupModel.cs
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyBackendApp.ViewModels
+{
+    public class CreateGroupModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public List<string> InvitedUserIds { get; set; } = new List<string>();
+    }
+}

f26fa304593608fb07b3f7a631d768553399a187
Tern Yuka Følbæk <tfolbaek@gmail.com>
2024-11-01 07:18:31 +0100

    Reduce dashboard navbar tsx to notifications only

--- AnthraClient/client/web/src/Dashboard/Components/DashboardNavbar/DashboardNavbar.tsx
+++ AnthraClient/client/web/src/Dashboard/Components/DashboardNavbar/DashboardNavbar.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { FaUser } from 'react-icons/fa';
 import './DashboardNavbar.css';
 import Notifications from '../Notifications/Notifications';
 
@@ -8,22 +6,9 @@ const DashboardNavbar: React.FC = () => {
 
 
     return (
-        <nav className="dashboard-navbar">
-            <div className="dashboard-navbar__container">
-                <div className="dashboard-navbar__logo">
-                    <h1>Anthra</h1>
-                </div>
-                <div className="flex gap-2">
-                    <Notifications />
-                    <div className="dashboard-navbar__profile-icon">
-                        <NavLink to="/profile">
-                            <FaUser className="profile-icon"/>
-                        </NavLink>
-                    </div>
-                </div>
-
-            </div>
-        </nav>
+        <div className="dashboard-navbar">
+            <Notifications />
+        </div>
     );
 };
 

02f877e6c6f8ec5b5fef4351af50d1b29890c3b0
Tern Yuka Følbæk <tfolbaek@gmail.com>
2024-11-01 07:30:24 +0100

    Add pfp to sidebar

--- AnthraClient/client/web/src/Dashboard/Components/Sidebar/Sidebar.tsx
+++ AnthraClient/client/web/src/Dashboard/Components/Sidebar/Sidebar.tsx
@@ -1,70 +1,82 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { FaSearch, FaUsers, FaEnvelope, FaCog, FaUsersCog, FaBuilding, FaHandHoldingHeart} from 'react-icons/fa';
+import {
+    FaSearch,
+    FaUsers,
+    FaFacebookMessenger,
+    FaCog,
+    FaBuilding,
+    FaHandHoldingHeart,
+    FaUserFriends,
+} from 'react-icons/fa';
 import './Sidebar.css';
 
 const Sidebar: React.FC = () => {
+    const userProfilePictureSrc = localStorage.getItem('userProfilePicture') || undefined;
+
     return (
         <div className="sidebar">
-            <NavLink
-                to="/explore"
-                className={({ isActive }) => `sidebar-link ${isActive ? 'active-link' : ''}`}
-                data-title="Explore"
-            >
-                <FaSearch className="sidebar-icon" />
-                Explore Page
-            </NavLink>
-            <NavLink
-                to="/messages"
-                className={({ isActive }) => `sidebar-link ${isActive ? 'active-link' : ''}`}
-                data-title="Messages"
-            >
-                <FaEnvelope className="sidebar-icon" />
-                Messages
-            </NavLink>
-            <NavLink
-                to="/connections"
-                className={({ isActive }) => `sidebar-link ${isActive ? 'active-link' : ''}`}
-                data-title="Connections"
-            >
-                <FaUsers className="sidebar-icon" />
-                Connections
-            </NavLink>
-
-            <NavLink
-                to="/groups"
-                className={({ isActive }) => `sidebar-link ${isActive ? 'active-link' : ''}`}
-                data-title="Groups"
-            >
-                <FaUsersCog className="sidebar-icon" />
-                Groups
-            </NavLink>
-            <NavLink
-                to="/facilities"
-                className={({ isActive }) => `sidebar-link ${isActive ? 'active-link' : ''}`}
-                data-title="Facilities"
-            >
-                <FaBuilding className="sidebar-icon" />
-                Facilities
-            </NavLink>
-            <NavLink
-                to="/requests"
-                className={({ isActive }) => `sidebar-link ${isActive ? 'active-link' : ''}`}
-                data-title="Requests"
-            >
-                <FaHandHoldingHeart className="sidebar-icon" />
-                Requests
-            </NavLink>
-            <NavLink
-                to="/settings"
-                className={({ isActive }) => `sidebar-link ${isActive ? 'active-link' : ''}`}
-                data-title="Settings"
-            >
-                <FaCog className="sidebar-icon" />
-                Settings
+            <div>
+                <NavLink
+                    to="/explore"
+                    className={({ isActive }) => `sidebar-link ${isActive ? 'active-link' : ''}`}
+                    data-title="Explore"
+                >
+                    <FaSearch className="sidebar-icon" />
+                </NavLink>
+                <NavLink
+                    to="/messages"
+                    className={({ isActive }) => `sidebar-link ${isActive ? 'active-link' : ''}`}
+                    data-title="Messages"
+                >
+                    <FaFacebookMessenger className="sidebar-icon" />
+                </NavLink>
+                <NavLink
+                    to="/connections"
+                    className={({ isActive }) => `sidebar-link ${isActive ? 'active-link' : ''}`}
+                    data-title="Connections"
+                >
+                    <FaUserFriends className="sidebar-icon" />
+                </NavLink>
+                <NavLink
+                    to="/groups"
+                    className={({ isActive }) => `sidebar-link ${isActive ? 'active-link' : ''}`}
+                    data-title="Groups"
+                >
+                    <FaUsers className="sidebar-icon" />
+                </NavLink>
+                <NavLink
+                    to="/facilities"
+                    className={({ isActive }) => `sidebar-link ${isActive ? 'active-link' : ''}`}
+                    data-title="Facilities"
+                >
+                    <FaBuilding className="sidebar-icon" />
+                </NavLink>
+                <NavLink
+                    to="/requests"
+                    className={({ isActive }) => `sidebar-link ${isActive ? 'active-link' : ''}`}
+                    data-title="Requests"
+                >
+                    <FaHandHoldingHeart className="sidebar-icon" />
+                </NavLink>
+                <NavLink
+                    to="/settings"
+                    className={({ isActive }) => `sidebar-link ${isActive ? 'active-link' : ''}`}
+                    data-title="Settings"
+                >
+                    <FaCog className="sidebar-icon" />
+                </NavLink>
+            </div>
+            <NavLink to="/profile">
+                <img
+                    src={userProfilePictureSrc}
+                    alt="profile picture"
+                    className="sidebar-profile-picture"
+                />
             </NavLink>
         </div>
     );
 };
+
 
 export default Sidebar;

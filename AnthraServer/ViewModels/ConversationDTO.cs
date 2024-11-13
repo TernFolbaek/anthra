430b6e9b4c72b03ff53fa9f7921d8bff45c5dfbf
Tern Yuka Følbæk <tfolbaek@gmail.com>
2024-11-13 20:04:41 +0100

    Add first name to conversationDTO

--- AnthraServer/ViewModels/ConversationDTO.cs
+++ AnthraServer/ViewModels/ConversationDTO.cs
@@ -2,7 +2,8 @@ namespace MyBackendApp.ViewModels
 {
     public class ConversationDTO
     {
-        public string UserId { get; set; } // ID of the other user
+        public string UserId { get; set; } 
+        public string FirstName { get; set; }
         public string UserName { get; set; }
         public string UserEmail { get; set; }
         public string UserProfilePicture { get; set; }

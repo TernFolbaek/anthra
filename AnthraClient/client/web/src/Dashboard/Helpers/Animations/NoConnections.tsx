d07effa1d32d3e3ad06384a768344b05d829de41
Tern Yuka Følbæk <tfolbaek@gmail.com>
2024-11-17 12:44:10 +0100

    Component for when the user has no connections it will then render a rive file

---
+++ AnthraClient/client/web/src/Dashboard/Helpers/Animations/NoConnections.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {useRive} from "@rive-app/react-canvas";
+import { useNavigate } from 'react-router-dom';
+import {FaMagnifyingGlass} from "react-icons/fa6";
+
+const NoConnectionsRive: React.FC = () => {
+    const navigate = useNavigate();
+    const { rive, RiveComponent } = useRive({
+        src: 'no_conversations.riv',
+        autoplay: true,
+    });
+
+    return (
+        <div className="no-connections-container">
+            <p className="text-xl text-center font-bold text-gray-700 mb-2">Send connections to create connections</p>
+            <button className="explore-button" onClick={() => navigate('/explore')}> <FaMagnifyingGlass/>Explore</button>
+            <RiveComponent className="no-connections-rive"/>
+        </div>
+    )
+}
+
+export default NoConnectionsRive;

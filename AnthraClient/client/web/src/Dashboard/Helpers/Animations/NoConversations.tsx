f51b1493bc732b78d4d59e7a8e161a5e24f937a3
Tern Yuka Følbæk <tfolbaek@gmail.com>
2024-11-17 12:44:36 +0100

    Create component for rive animation when the user has no current conversations

---
+++ AnthraClient/client/web/src/Dashboard/Helpers/Animations/NoConversations.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {useRive} from "@rive-app/react-canvas";
+import { useNavigate } from 'react-router-dom';
+import {FaMagnifyingGlass} from "react-icons/fa6";
+
+const NoConversationsRive: React.FC = () => {
+    const navigate = useNavigate();
+    const { rive, RiveComponent } = useRive({
+        src: 'juggling_master.riv',
+        autoplay: true,
+    });
+
+    return (
+        <div className="no-connections-container">
+            <RiveComponent className="no-conversations-rive"/>
+            <p className="text-m font-bold text-gray-500 text-center mb-2">Begin a conversation with a connection</p>
+            <button className="explore-button" onClick={() => navigate('/connections')}><FaMagnifyingGlass/>Connections
+            </button>
+        </div>
+    )
+}
+
+export default NoConversationsRive;

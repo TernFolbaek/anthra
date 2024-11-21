bbc2378abf608ff560480077587e3288684b1c94
Tern Yuka Følbæk <tfolbaek@gmail.com>
2024-11-21 15:26:46 +0100

    Center rive animation

--- AnthraClient/client/web/src/Dashboard/Helpers/Animations/NoMoreUsersToExplore.tsx
+++ AnthraClient/client/web/src/Dashboard/Helpers/Animations/NoMoreUsersToExplore.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import {useRive} from "@rive-app/react-canvas";
 
 const NoMoreUsersToExplore: React.FC = () => {
-    const { rive, RiveComponent } = useRive({
+    const { RiveComponent } = useRive({
         src: '/rive/stasher_character.riv',
         autoplay: true,
     });
 
     return (
-        <div className="flex flex-col items-center">
-            <RiveComponent className="no-conversations-rive" />
-            <p className="text-m font-bold text-gray-500 text-center mb-2">Awaiting new users to join</p>
+        <div className="no-more-users-or-groups">
+                <RiveComponent className="no-conversations-rive ml-auto mr-auto"/>
+                <p className="text-m font-bold text-gray-500 text-center mb-2">Awaiting new users to join</p>
         </div>
     )
 }

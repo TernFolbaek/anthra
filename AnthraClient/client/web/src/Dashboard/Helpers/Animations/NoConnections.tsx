06e4abed2174515bb3081e8b463ab0e6218c9bc4
Tern Yuka Følbæk <tfolbaek@gmail.com>
2024-12-03 16:12:58 +0100

    Send connections to create connections -> Explore new connections

--- AnthraClient/client/web/src/Dashboard/Helpers/Animations/NoConnections.tsx
+++ AnthraClient/client/web/src/Dashboard/Helpers/Animations/NoConnections.tsx
@@ -12,7 +12,7 @@ const NoConnectionsRive: React.FC = () => {
 
     return (
         <div className="no-connections-container">
-            <p className="text-base text-center font-bold text-gray-500 mb-2">Send connections to create connections</p>
+            <p className="text-base text-center font-bold text-gray-500 mb-2">Explore new connections</p>
             <button className="explore-button" onClick={() => navigate('/explore')}> <FaMagnifyingGlass/>Explore</button>
             <RiveComponent className="no-connections-rive"/>
         </div>

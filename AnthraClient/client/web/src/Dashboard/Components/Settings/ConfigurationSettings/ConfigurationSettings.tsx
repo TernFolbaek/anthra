08d4ba770d73267cc5879d52b3faa4e789e8e1ee
Tern Yuka Følbæk <tfolbaek@gmail.com>
2024-12-09 21:45:35 +0100

    Refactor from dark-mode to dark to support tailwind dark selector

--- AnthraClient/client/web/src/Dashboard/Components/Settings/ConfigurationSettings/ConfigurationSettings.tsx
+++ AnthraClient/client/web/src/Dashboard/Components/Settings/ConfigurationSettings/ConfigurationSettings.tsx
@@ -2,33 +2,33 @@ import React, { useState, useEffect } from 'react';
 import './ConfigurationSettings.css';
 
 const ConfigurationSettings: React.FC = () => {
-    // Parse the value from localStorage and default to false if it's null
     const storedDarkMode = localStorage.getItem('isDark') === 'true';
     const [darkMode, setDarkMode] = useState(storedDarkMode);
 
     const toggleDarkMode = () => {
         setDarkMode((prevMode) => {
             const newMode = !prevMode;
-            // Update localStorage when the mode changes
+            // Update localStorage
             localStorage.setItem('isDark', newMode.toString());
-            document.body.classList.toggle('dark-mode', newMode);
+            // Toggle the 'dark' class on the html element
+            document.body.classList.toggle('dark', newMode);
             return newMode;
         });
     };
 
     useEffect(() => {
-        // Ensure the body class reflects the current dark mode setting on load
+        // Ensure the html class reflects the current dark mode setting on load
         if (darkMode) {
-            document.body.classList.add('dark-mode');
+            document.body.classList.add('dark');
         } else {
-            document.body.classList.remove('dark-mode');
+            document.body.classList.remove('dark');
         }
     }, [darkMode]);
 
     return (
         <div className="configuration-settings">
             <div className="toggle-group">
-                <label>Dark Mode:</label>
+                <label className="dark:text-red-500">Dark Mode:</label>
                 <div className="toggle-switch">
                     <input
                         type="checkbox"

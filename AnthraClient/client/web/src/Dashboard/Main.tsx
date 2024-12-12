90105d3c5ce865d70bdfacf9d1e1cd3c279004ce
Tern Yuka Følbæk <tfolbaek@gmail.com>
2024-12-12 15:18:41 +0100

    won't render footer in message container, personal or in groups

--- AnthraClient/client/web/src/Dashboard/Main.tsx
+++ AnthraClient/client/web/src/Dashboard/Main.tsx
@@ -1,6 +1,6 @@
 // src/main.tsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Sidebar from './Components/Sidebar/Sidebar';
 import ExplorePage from './Components/ExplorePage/ExplorePage';
 import Connections from './Components/Connections/Connections';
@@ -14,35 +14,50 @@ import GroupsLayout from './Layouts/GroupsLayout/GroupsLayout';
 import MessageOptionalLayout from "./Layouts/MessageOptionalLayout/MessageOptionalLayout";
 import './Main.css';
 
+const DashboardContent: React.FC = () => {
+    const location = useLocation();
+    const isMobile = window.innerWidth <= 480;
+
+    // Check if the path matches "/messages/:userId" or "/groups/:groupId"
+    const isUserSpecificMessage = /^\/messages\/[^/]+$/.test(location.pathname);
+    const isGroupSpecificPage = /^\/groups\/[^/]+$/.test(location.pathname);
+
+    // If on a specific user/group page and on mobile, hide the footer
+    const shouldHideFooter = isMobile && (isUserSpecificMessage || isGroupSpecificPage);
+
+    return (
+        <div className="dashboard-container">
+            <div className="content-wrapper">
+                <Sidebar />
+                <div className="main-content">
+                    <Routes>
+                        <Route element={<MessagesLayout />}>
+                            <Route path="/messages" element={<Messages />} />
+                            <Route path="/messages/:userId" element={<Messages />} />
+                        </Route>
+                        <Route path="/groups" element={<GroupsLayout />}>
+                            <Route index element={<Groups />} />
+                            <Route path=":groupId" element={<Groups />} />
+                        </Route>
+                        <Route element={<MessageOptionalLayout/>}>
+                            <Route path="/connections" element={<Connections />} />
+                            <Route path="/settings" element={<Settings />} />
+                        </Route>
+                        <Route path="/" element={<ExplorePage />} />
+                        <Route path="/explore" element={<ExplorePage />} />
+                        <Route path="/profile" element={<Profile />} />
+                    </Routes>
+                </div>
+            </div>
+            {!shouldHideFooter && <Footer />}
+        </div>
+    );
+};
+
 const Dashboard: React.FC = () => {
     return (
         <Router>
-            <div className="dashboard-container">
-                <div className="content-wrapper">
-                    <Sidebar />
-                    <div className="main-content">
-                        <Routes>
-                            <Route element={<MessagesLayout />}>
-                                <Route path="/messages" element={<Messages />} />
-                                <Route path="/messages/:userId" element={<Messages />} />
-                            </Route>
-                            <Route path="/groups" element={<GroupsLayout />}>
-                                <Route index element={<Groups />} />
-                                <Route path=":groupId" element={<Groups />} />
-                            </Route>
-                            <Route element={<MessageOptionalLayout/>}>
-                                <Route path="/connections" element={<Connections />} />
-                                <Route path="/settings" element={<Settings />} />
-                            </Route>
-                            <Route path="/" element={<ExplorePage />} />
-                            <Route path="/explore" element={<ExplorePage />} />
-                            <Route path="/profile" element={<Profile />} />
-
-                        </Routes>
-                    </div>
-                </div>
-                <Footer />
-            </div>
+            <DashboardContent />
         </Router>
     );
 };

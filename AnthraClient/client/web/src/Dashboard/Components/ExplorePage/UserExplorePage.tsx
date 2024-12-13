bba7e2b78a597d27ff0b82fc1a302739556977c0
Tern Yuka Følbæk <tfolbaek@gmail.com>
2024-12-13 23:35:29 +0100

    Far better styling for mobile view where each card is two pages long

--- AnthraClient/client/web/src/Dashboard/Components/ExplorePage/UserExplorePage.tsx
+++ AnthraClient/client/web/src/Dashboard/Components/ExplorePage/UserExplorePage.tsx
@@ -1,10 +1,10 @@
-// UserExplorePage.tsx
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import './UserExplorePage.css';
 import NoMoreUsersToExplore from '../../Helpers/Animations/NoMoreUsersToExplore';
 import Snackbar from "../../Helpers/Snackbar/Snackbar";
 import ReferModal from './ReferModal/ReferModal';
+import useWindowWidth from '../../hooks/useWindowWidth'; // Adjust the import path as needed
 
 interface Course {
     courseName: string;
@@ -20,7 +20,7 @@ interface User {
     work: string;
     courses: Course[];
     subjects: string[];
-    statuses: string[]; // New property for user statuses
+    statuses: string[];
     aboutMe: string;
     age: number;
     profilePictureUrl: string;
@@ -37,6 +37,14 @@ const UserExplorePage: React.FC = () => {
     const [snackbarMessage, setSnackbarMessage] = useState<string>('');
 
     const [showReferModal, setShowReferModal] = useState(false);
+
+    const windowWidth = useWindowWidth();
+    const isSmallScreen = windowWidth < 480;
+
+    const [currentPage, setCurrentPage] = useState<number>(1); // 1 or 2
+    const [shake, setShake] = useState<boolean>(false);
+
+    const cardRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         const fetchUsers = async () => {
@@ -69,7 +77,7 @@ const UserExplorePage: React.FC = () => {
             try {
                 await axios.post(
                     'http://localhost:5001/api/Connections/SendRequest',
-                    {targetUserId: currentUser.id},
+                    { targetUserId: currentUser.id },
                     {
                         headers: {
                             Authorization: `Bearer ${token}`,
@@ -84,6 +92,7 @@ const UserExplorePage: React.FC = () => {
             }
         }
         setCurrentIndex(currentIndex + 1);
+        setCurrentPage(1); // Reset to first page for next user
     };
 
     const handleSkip = async () => {
@@ -91,7 +100,7 @@ const UserExplorePage: React.FC = () => {
             try {
                 await axios.post(
                     'http://localhost:5001/api/Explore/SkipUser',
-                    {UserIdToSkip: currentUser.id},
+                    { UserIdToSkip: currentUser.id },
                     {
                         headers: {
                             Authorization: `Bearer ${token}`,
@@ -103,89 +112,218 @@ const UserExplorePage: React.FC = () => {
             }
         }
         setCurrentIndex(currentIndex + 1);
+        setCurrentPage(1); // Reset to first page for next user
     };
+
+    const handlePageChange = (direction: 'left' | 'right') => {
+        if (!isSmallScreen || !currentUser) return;
+
+        if (direction === 'left') {
+            if (currentPage === 1) {
+                triggerShake();
+            } else {
+                setCurrentPage(1);
+            }
+        } else if (direction === 'right') {
+            if (currentPage === 2) {
+                triggerShake();
+            } else {
+                setCurrentPage(2);
+            }
+        }
+    };
+
+    const triggerShake = () => {
+        setShake(true);
+        setTimeout(() => setShake(false), 500); // Duration of the shake animation
+    };
+
+    const handleLeftClick = () => handlePageChange('left');
+    const handleRightClick = () => handlePageChange('right');
 
     return (
         <div className="user-explore-container">
             {currentUser ? (
-                <div className="explore-user-card">
-                    <div className="flex flex-col">
-                        <div className="explore-user-card-content">
-                            <div className="flex items-center gap-2">
-                                <img
-                                    className="explore-user-card-img"
-                                    src={`${currentUser.profilePictureUrl}`}
-                                    alt="Profile"
-                                />
-                                <div className="flex flex-col">
-                                    <h2 className="user-name">
-                                        {currentUser.firstName} {currentUser.lastName}, {currentUser.age}
-                                    </h2>
-                                    <p className="user-location">{currentUser.location}</p>
-                                </div>
+                <div
+                    className={`explore-user-card ${isSmallScreen ? 'small-screen' : ''} ${shake ? 'shake' : ''}`}
+                    ref={cardRef}
+                >
+                    {isSmallScreen ? (
+                        <div className="small-screen-content">
+                            {/* Horizontal Indicators */}
+                            <div className="page-indicators">
+                                <div className={`indicator ${currentPage === 1 ? 'active' : ''}`}></div>
+                                <div className={`indicator ${currentPage === 2 ? 'active' : ''}`}></div>
                             </div>
-                            <div className="user-info">
-                                <h3>Institution</h3>
-                                <p>{currentUser.institution}</p>
-                                <h3>Work</h3>
-                                <p>{currentUser.work}</p>
-                                <h3>About Me</h3>
-                                <p>{currentUser.aboutMe}</p>
-
-                                {currentUser.subjects && currentUser.subjects.length > 0 && (
-                                    <div>
-                                        <h3>Subjects</h3>
-                                        <p>{currentUser.subjects.join(', ')}</p>
-                                    </div>
-                                )}
-
-                                {currentUser.courses && currentUser.courses.length > 0 && (
-                                    <div>
-                                        <h3>Courses</h3>
-                                        <ul className="user-explore-courses-list">
-                                            {currentUser.courses.map((course, index) => (
-                                                <li key={index}>
-                                                    <a href={course.courseLink} target="_blank"
-                                                       rel="noopener noreferrer">
-                                                        {course.courseName}
-                                                    </a>
-                                                </li>
-                                            ))}
-                                        </ul>
-                                    </div>
-                                )}
-
-                                {currentUser.statuses && currentUser.statuses.length > 0 && (
-                                    <div className="user-explore-statuses">
-                                        <h3>Status</h3>
-                                        <div className="flex gap-2">                                        {currentUser.statuses.map((st, i) => (
-                                            <p
-                                                key={i}
-                                                className="status-tag-explore"
-                                            >{st}
-                                            </p>
-                                        ))}
+                            {/* Click Areas */}
+                            <div className="click-overlay">
+                                <div className="click-area left" onClick={handleLeftClick}></div>
+                                <div className="click-area right" onClick={handleRightClick}></div>
+                            </div>
+                            {/* Page Content */}
+                            {currentPage === 1 ? (
+                                <div className="page-content page-1">
+                                    <div className="flex items-center gap-2">
+                                        <img
+                                            className="explore-user-card-img"
+                                            src={`${currentUser.profilePictureUrl}`}
+                                            alt="Profile"
+                                        />
+                                        <div className="flex flex-col">
+                                            <h2 className="user-name">
+                                                {currentUser.firstName} {currentUser.lastName}, {currentUser.age}
+                                            </h2>
+                                            <p className="user-location">{currentUser.location}</p>
                                         </div>
                                     </div>
-                                )}
+                                    <div className="user-info">
+                                        <h3>Institution</h3>
+                                        <p>{currentUser.institution}</p>
+                                        <h3>About Me</h3>
+                                        <p>{currentUser.aboutMe}</p>
+                                    </div>
+                                </div>
+                            ) : (
+                                <div className="page-content page-2">
+                                    <div className="user-info">
+                                        {currentUser.subjects && currentUser.subjects.length > 0 && (
+                                            <div>
+                                                <h3>Subjects</h3>
+                                                <p>{currentUser.subjects.join(', ')}</p>
+                                            </div>
+                                        )}
+
+                                        {currentUser.courses && currentUser.courses.length > 0 && (
+                                            <div>
+                                                <h3>Courses</h3>
+                                                <ul className="user-explore-courses-list">
+                                                    {currentUser.courses.map((course, index) => (
+                                                        <li key={index}>
+                                                            <a href={course.courseLink} target="_blank"
+                                                               rel="noopener noreferrer">
+                                                                {course.courseName}
+                                                            </a>
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                            </div>
+                                        )}
+
+                                        {currentUser.statuses && currentUser.statuses.length > 0 && (
+                                            <div className="user-explore-statuses">
+                                                <h3>Status</h3>
+                                                <div className="flex gap-2">
+                                                    {currentUser.statuses.map((st, i) => (
+                                                        <p
+                                                            key={i}
+                                                            className="status-tag-explore"
+                                                        >{st}
+                                                        </p>
+                                                    ))}
+                                                </div>
+                                            </div>
+                                        )}
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    ) : (
+                        // Existing layout for larger screens
+                        <div className="flex flex-col">
+                            <div className="explore-user-card-content">
+                                <div className="flex items-center gap-2">
+                                    <img
+                                        className="explore-user-card-img"
+                                        src={`${currentUser.profilePictureUrl}`}
+                                        alt="Profile"
+                                    />
+                                    <div className="flex flex-col">
+                                        <h2 className="user-name">
+                                            {currentUser.firstName} {currentUser.lastName}, {currentUser.age}
+                                        </h2>
+                                        <p className="user-location">{currentUser.location}</p>
+                                    </div>
+                                </div>
+                                <div className="user-info">
+                                    <h3>Institution</h3>
+                                    <p>{currentUser.institution}</p>
+                                    <h3>Work</h3>
+                                    <p>{currentUser.work}</p>
+                                    <h3>About Me</h3>
+                                    <p>{currentUser.aboutMe}</p>
+
+                                    {currentUser.subjects && currentUser.subjects.length > 0 && (
+                                        <div>
+                                            <h3>Subjects</h3>
+                                            <p>{currentUser.subjects.join(', ')}</p>
+                                        </div>
+                                    )}
+
+                                    {currentUser.courses && currentUser.courses.length > 0 && (
+                                        <div>
+                                            <h3>Courses</h3>
+                                            <ul className="user-explore-courses-list">
+                                                {currentUser.courses.map((course, index) => (
+                                                    <li key={index}>
+                                                        <a href={course.courseLink} target="_blank"
+                                                           rel="noopener noreferrer">
+                                                            {course.courseName}
+                                                        </a>
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                        </div>
+                                    )}
+
+                                    {currentUser.statuses && currentUser.statuses.length > 0 && (
+                                        <div className="user-explore-statuses">
+                                            <h3>Status</h3>
+                                            <div className="flex gap-2">
+                                                {currentUser.statuses.map((st, i) => (
+                                                    <p
+                                                        key={i}
+                                                        className="status-tag-explore"
+                                                    >{st}
+                                                    </p>
+                                                ))}
+                                            </div>
+                                        </div>
+                                    )}
+                                </div>
                             </div>
                         </div>
-
-                        <div className="user-explore-page-button-container">
-                            <button className="connect-button" onClick={handleConnect}>
-                                Connect
-                            </button>
-                            <button className="refer-button" onClick={() => setShowReferModal(true)}>
-                                Refer
-                            </button>
-                            <button className="skip-button" onClick={handleSkip}>
-                                Skip
-                            </button>
-                        </div>
-                    </div>
+                    )}
                 </div>
             ) : (
-                <NoMoreUsersToExplore/>
+                <NoMoreUsersToExplore />
+            )}
+
+            {!isSmallScreen && currentUser && (
+                <div className="user-explore-page-button-container">
+                    <button className="connect-button" onClick={handleConnect}>
+                        Connect
+                    </button>
+                    <button className="refer-button" onClick={() => setShowReferModal(true)}>
+                        Refer
+                    </button>
+                    <button className="skip-button" onClick={handleSkip}>
+                        Skip
+                    </button>
+                </div>
+            )}
+
+            {isSmallScreen && currentUser && (
+                <div className="user-explore-page-button-container">
+                    <button className="connect-button" onClick={handleConnect}>
+                        Connect
+                    </button>
+                    <button className="refer-button" onClick={() => setShowReferModal(true)}>
+                        Refer
+                    </button>
+                    <button className="skip-button" onClick={handleSkip}>
+                        Skip
+                    </button>
+                </div>
             )}
 
             {snackbarVisible && (
@@ -203,8 +341,7 @@ const UserExplorePage: React.FC = () => {
                     onClose={() => setShowReferModal(false)}
                 />
             )}
-        </div>
-    );
+        </div>)
 };
 
 export default UserExplorePage;

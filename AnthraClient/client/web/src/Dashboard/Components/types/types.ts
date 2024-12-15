ccb2b714520c24521b5977e579e14a585f71371f
Tern Yuka Følbæk <tfolbaek@gmail.com>
2024-12-15 22:59:19 +0100

    Create file for shared types

---
+++ AnthraClient/client/web/src/Dashboard/Components/types/types.ts
@@ -0,0 +1,48 @@
+// src/types/types.ts
+
+export enum InvitationActionType {
+    None = "None",
+    Skipped = "Skipped",
+    Connected = "Connected",
+    Accepted = "Accepted",
+    Declined = "Declined"
+}
+
+
+export interface Attachment {
+    id: number;
+    fileName: string;
+    fileUrl: string;
+}
+
+export interface Message {
+    id: number;
+    senderId: string;
+    receiverId: string;
+    content: string;
+    timestamp: string;
+    isGroupInvitation: boolean;
+    isReferralCard: boolean;
+    groupId: number | null;
+    groupName?: string;
+    attachments?: Attachment[];
+    invitationStatus?: boolean;
+    actionType: InvitationActionType;
+}
+
+export interface UserProfile {
+    id: string;
+    userName: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    location: string;
+    institution: string;
+    work: string;
+    courses: { courseName: string; courseLink: string }[];
+    subjects: string[];
+    aboutMe: string;
+    age: number;
+    profilePictureUrl: string;
+    createdProfile: Date;
+}

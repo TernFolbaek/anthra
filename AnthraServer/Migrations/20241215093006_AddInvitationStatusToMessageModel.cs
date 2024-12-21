d8f4af0b874c8a02f66e1b7ffdcd009207a66bc7
Tern Yuka Følbæk <tfolbaek@gmail.com>
2024-12-21 14:03:59 +0100

    Include snapshot

---
+++ AnthraServer/Migrations/20241215093006_AddInvitationStatusToMessageModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnthraBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInvitationStatusToMessageModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "InvitationStatus",
+                table: "Messages",
+                type: "boolean",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InvitationStatus",
+                table: "Messages");
+        }
+    }
+}

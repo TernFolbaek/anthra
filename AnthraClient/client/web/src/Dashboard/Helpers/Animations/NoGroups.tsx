6398f20750391b1bb2b5ecd1e0c7a0a5515b839b
Tern Yuka Følbæk <tfolbaek@gmail.com>
2024-12-31 11:05:07 +0100

    No groups text semi bold gray 400

--- AnthraClient/client/web/src/Dashboard/Helpers/Animations/NoGroups.tsx
+++ AnthraClient/client/web/src/Dashboard/Helpers/Animations/NoGroups.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import {useRive} from "@rive-app/react-canvas";
 
 const NoGroups: React.FC = () => {
 
     return (
         <div className="no-connections-container">
-            <p className="text-m font-bold dark:text-white text-center mb-2 ">Create a group with your connections</p>
+            <p className="text-m font-semibold text-gray-400 text-center mb-2 ">Create a group with your connections</p>
         </div>
     )
 }

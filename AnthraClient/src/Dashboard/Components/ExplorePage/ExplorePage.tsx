724b85af7a70a1d374066ea1e025f55f3ff37c3d
Tern Yuka Følbæk <tfolbaek@gmail.com>
2025-01-08 09:56:21 +0100

    Send on settings click prop

--- AnthraClient/src/Dashboard/Components/ExplorePage/ExplorePage.tsx
+++ AnthraClient/src/Dashboard/Components/ExplorePage/ExplorePage.tsx
@@ -4,14 +4,17 @@ import Menu from './ExploreToggle/Menu';
 import UserExplorePage from './IndividualExplore/UserExplorePage';
 import GroupExplorePage from './GroupExplore/GroupExplorePage';
 
-const ExplorePage: React.FC = () => {
+interface ExplorePageProps{
+    onSettingsClick: () => void;
+}
+const ExplorePage: React.FC<ExplorePageProps> = ({onSettingsClick}) => {
     const [activePage, setActivePage] = useState<'individuals' | 'groups'>('individuals');
 
     return (
         <div className="explore-page">
             <div  className="w-[100%] ">
                 <Menu activePage={activePage} setActivePage={setActivePage} />
-                {activePage === 'individuals' ? <UserExplorePage /> : <GroupExplorePage />}
+                {activePage === 'individuals' ? <UserExplorePage onSettingsClick={onSettingsClick} /> : <GroupExplorePage />}
             </div>
         </div>
     );

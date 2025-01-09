667c9c8c0fc6f04796b7dfee8276e8b58bbd9a58
Tern Yuka Følbæk <tfolbaek@gmail.com>
2025-01-09 12:10:13 +0100

    Remove rive component

--- AnthraClient/src/Dashboard/Helpers/Animations/NoMoreGroupsToExplore.tsx
+++ AnthraClient/src/Dashboard/Helpers/Animations/NoMoreGroupsToExplore.tsx
@@ -1,16 +1,9 @@
 import React from "react";
-import {useRive} from "@rive-app/react-canvas";
 
 const NoMoreGroupsToExplore: React.FC = () => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const { RiveComponent } = useRive({
-        src: '/rive/stasher_character.riv',
-        autoplay: true,
-    });
 
     return (
         <div className="no-more-users-or-groups">
-            <RiveComponent className="no-conversations-rive ml-auto mr-auto"/>
             <p className="dark:text-white text-m font-bold text-gray-500 text-center mb-2 ">Awaiting new groups</p>
         </div>
     )
--- AnthraClient/src/Dashboard/Helpers/Animations/NoMoreUsersToExplore.tsx
+++ AnthraClient/src/Dashboard/Helpers/Animations/NoMoreUsersToExplore.tsx
@@ -1,15 +1,9 @@
 import React from "react";
-import {useRive} from "@rive-app/react-canvas";
 
 const NoMoreUsersToExplore: React.FC = () => {
-    const { RiveComponent } = useRive({
-        src: '/rive/stasher_character.riv',
-        autoplay: true,
-    });
 
     return (
         <div className="no-more-users-or-groups">
-                <RiveComponent className="no-conversations-rive ml-auto mr-auto"/>
                 <p className="dark:text-white text-m font-bold text-gray-500 text-center mb-2 ">Awaiting new users to join</p>
         </div>
     )

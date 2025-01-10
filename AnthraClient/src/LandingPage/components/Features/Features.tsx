982cc5385414e13f81199018871e6b388491c21c
Tern Yuka Følbæk <tfolbaek@gmail.com>
2025-01-10 23:29:22 +0100

    Remove features

--- AnthraClient/src/LandingPage/components/Features/Features.tsx
+++ AnthraClient/src/LandingPage/components/Features/Features.tsx
@@ -4,18 +4,7 @@ import './Features.css';
 const Features: React.FC = () => {
     return (
         <div className="feature-showcase-wrapper">
-            <h1 className="demo-video-title">Demo</h1>
-            <div className="demo-video-container">
-                <iframe
-                    className="demo-video-iframe"
-                    src="https://www.youtube.com/embed/B_pltPlrEfY"
-                    title="Demo Video"
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                ></iframe>
 
-            </div>
         </div>
     );
 };

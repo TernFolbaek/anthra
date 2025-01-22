cb0eab81005efa5aec2b5583f7df9f5ca6cb4d16
Tern Yuka Følbæk <tfolbaek@gmail.com>
2025-01-22 21:07:36 +0100

    Delete messages endpoint

--- AnthraServer/Controllers/Group/GroupMessagesController.cs
+++ AnthraServer/Controllers/Group/GroupMessagesController.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using System.IO;
+using System.Security.Claims;
 using System.Text;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models; 
@@ -25,7 +26,7 @@ namespace MyBackendApp.Controllers
         private readonly ApplicationDbContext _context;
         private readonly IHubContext<ChatHub> _hubContext;
         private readonly IHubContext<NotificationHub> _notificationHub;
-        
+
         private readonly string _storageConnectionString =
             "BlobEndpoint=https://anthra.blob.core.windows.net/;QueueEndpoint=https://anthra.queue.core.windows.net/;FileEndpoint=https://anthra.file.core.windows.net/;TableEndpoint=https://anthra.table.core.windows.net/;SharedAccessSignature=sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2026-10-01T16:15:49Z&st=2024-10-01T08:15:49Z&spr=https&sig=SHmifWmLLf50pO0nqEVnIBYTqRx0QHmJpS5iAiYXq%2F0%3D";
 
@@ -44,101 +45,101 @@ namespace MyBackendApp.Controllers
             _hubContext = hubContext;
             // _logger = logger;
         }
-        
+
         [HttpGet("GetGroupChatHistory")]
-public async Task<IActionResult> GetGroupChatHistory(int groupId, int pageSize = 30, string nextToken = null)
-{
-    var query = _context.GroupMessages
-        .Include(m => m.Sender)
-        .Include(m => m.Attachment)
-        .Where(m => m.GroupId == groupId);
-
-    // If nextToken is provided, decode it to know what "page" to fetch:
-    if (!string.IsNullOrEmpty(nextToken))
-    {
-        try
+        public async Task<IActionResult> GetGroupChatHistory(int groupId, int pageSize = 30, string nextToken = null)
         {
-            var decodedBytes = Convert.FromBase64String(nextToken);
-            var decodedString = Encoding.UTF8.GetString(decodedBytes);
-            var token = JsonConvert.DeserializeObject<MessagesController.NextToken>(decodedString);
+            var query = _context.GroupMessages
+                .Include(m => m.Sender)
+                .Include(m => m.Attachment)
+                .Where(m => m.GroupId == groupId);
 
-            if (token != null)
+            // If nextToken is provided, decode it to know what "page" to fetch:
+            if (!string.IsNullOrEmpty(nextToken))
             {
-                // Return messages strictly older than the last one we had
-                query = query.Where(m => 
-                    m.Timestamp < token.Timestamp ||
-                    (m.Timestamp == token.Timestamp && m.Id < token.Id));
-            }
-            else
-            {
-                return BadRequest("Invalid nextToken.");
-            }
-        }
-        catch (FormatException)
-        {
-            return BadRequest("Invalid nextToken format.");
-        }
-        catch (JsonException)
-        {
-            return BadRequest("Invalid nextToken content.");
-        }
-    }
-    
-    query = query.OrderByDescending(m => m.Timestamp)
-                 .ThenByDescending(m => m.Id);
-
-    var fetchedMessages = await query
-        .Take(pageSize)
-        .Select(m => new
-        {
-            m.Id,
-            m.Content,
-            m.Timestamp,
-            SenderId = m.SenderId,
-            SenderFirstName = m.Sender != null ? m.Sender.FirstName : "Deleted User",
-            SenderProfilePictureUrl = m.Sender != null ? m.Sender.ProfilePictureUrl : null,
-            GroupId = m.GroupId,
-            Attachments = m.Attachment != null
-                ? new[]
+                try
                 {
-                    new
+                    var decodedBytes = Convert.FromBase64String(nextToken);
+                    var decodedString = Encoding.UTF8.GetString(decodedBytes);
+                    var token = JsonConvert.DeserializeObject<MessagesController.NextToken>(decodedString);
+
+                    if (token != null)
                     {
-                        m.Attachment.Id,
-                        m.Attachment.FileName,
-                        m.Attachment.FileUrl
+                        // Return messages strictly older than the last one we had
+                        query = query.Where(m =>
+                            m.Timestamp < token.Timestamp ||
+                            (m.Timestamp == token.Timestamp && m.Id < token.Id));
+                    }
+                    else
+                    {
+                        return BadRequest("Invalid nextToken.");
                     }
                 }
-                : null
-        })
-        .ToListAsync();
+                catch (FormatException)
+                {
+                    return BadRequest("Invalid nextToken format.");
+                }
+                catch (JsonException)
+                {
+                    return BadRequest("Invalid nextToken content.");
+                }
+            }
 
-    // Determine if more data might remain
-    string newNextToken = null;
-    if (fetchedMessages.Count == pageSize)
-    {
-        // The last fetched message (which is oldest in this chunk)
-        var lastMessage = fetchedMessages.Last();
-        var tokenObject = new MessagesController.NextToken
-        {
-            Timestamp = lastMessage.Timestamp,
-            Id = lastMessage.Id
-        };
-        var tokenJson = JsonConvert.SerializeObject(tokenObject);
-        newNextToken = Convert.ToBase64String(Encoding.UTF8.GetBytes(tokenJson));
-    }
+            query = query.OrderByDescending(m => m.Timestamp)
+                .ThenByDescending(m => m.Id);
 
-    // Reverse back to chronological order
-    var messages = fetchedMessages
-        .OrderBy(m => m.Timestamp)
-        .ThenBy(m => m.Id)
-        .ToList();
+            var fetchedMessages = await query
+                .Take(pageSize)
+                .Select(m => new
+                {
+                    m.Id,
+                    m.Content,
+                    m.Timestamp,
+                    SenderId = m.SenderId,
+                    SenderFirstName = m.Sender != null ? m.Sender.FirstName : "Deleted User",
+                    SenderProfilePictureUrl = m.Sender != null ? m.Sender.ProfilePictureUrl : null,
+                    GroupId = m.GroupId,
+                    Attachments = m.Attachment != null
+                        ? new[]
+                        {
+                            new
+                            {
+                                m.Attachment.Id,
+                                m.Attachment.FileName,
+                                m.Attachment.FileUrl
+                            }
+                        }
+                        : null
+                })
+                .ToListAsync();
 
-    return Ok(new
-    {
-        messages,
-        nextToken = newNextToken
-    });
-}
+            // Determine if more data might remain
+            string newNextToken = null;
+            if (fetchedMessages.Count == pageSize)
+            {
+                // The last fetched message (which is oldest in this chunk)
+                var lastMessage = fetchedMessages.Last();
+                var tokenObject = new MessagesController.NextToken
+                {
+                    Timestamp = lastMessage.Timestamp,
+                    Id = lastMessage.Id
+                };
+                var tokenJson = JsonConvert.SerializeObject(tokenObject);
+                newNextToken = Convert.ToBase64String(Encoding.UTF8.GetBytes(tokenJson));
+            }
+
+            // Reverse back to chronological order
+            var messages = fetchedMessages
+                .OrderBy(m => m.Timestamp)
+                .ThenBy(m => m.Id)
+                .ToList();
+
+            return Ok(new
+            {
+                messages,
+                nextToken = newNextToken
+            });
+        }
 
 
         [HttpPost("SendGroupMessage")]
@@ -171,7 +172,7 @@ public async Task<IActionResult> GetGroupChatHistory(int groupId, int pageSize =
             {
                 // Save the file in a subfolder corresponding to the groupId
                 var fileUrl = await SaveFileToAzureAsync(model.File, model.GroupId);
-                
+
                 var attachment = new Attachment
                 {
                     FileName = model.File.FileName,
@@ -269,17 +270,102 @@ public async Task<IActionResult> GetGroupChatHistory(int groupId, int pageSize =
                 SenderFirstName = sender?.FirstName,
                 SenderProfilePictureUrl = sender?.ProfilePictureUrl,
                 GroupId = message.GroupId,
-                Attachments = message.Attachment != null ? new[] {
-                    new {
-                        message.Attachment.Id,
-                        message.Attachment.FileName,
-                        message.Attachment.FileUrl
+                Attachments = message.Attachment != null
+                    ? new[]
+                    {
+                        new
+                        {
+                            message.Attachment.Id,
+                            message.Attachment.FileName,
+                            message.Attachment.FileUrl
+                        }
                     }
-                } : null
+                    : null
             });
 
             return Ok();
         }
+         [HttpDelete("DeleteMessage")]
+        public async Task<IActionResult> DeleteMessage([FromBody] DeleteGroupMessageRequest request)
+        {
+            if (request == null || request.MessageId <= 0)
+            {
+                return BadRequest("Invalid messageId.");
+            }
+
+            var currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+
+            var message = await _context.GroupMessages
+                .Include(m => m.Attachment)
+                .FirstOrDefaultAsync(m => m.Id == request.MessageId);
+
+            if (message == null)
+            {
+                return NotFound("Message does not exist or was already deleted.");
+            }
+
+            var group = await _context.Groups
+                .Include(g => g.Creator)
+                .FirstOrDefaultAsync(g => g.Id == message.GroupId);
+
+            bool isSender = (message.SenderId == currentUserId);
+            bool isGroupCreator = (group?.CreatorId == currentUserId);
+            
+
+            // If there's an attachment, decide if you want to delete it from Blob Storage
+            if (message.Attachment != null)
+            {
+                try
+                {
+                    var blobServiceClient = new BlobServiceClient(_storageConnectionString);
+                    var containerClient = blobServiceClient.GetBlobContainerClient(_groupMessagesContainerName);
+
+                    // The path is typically something like "groupId/fileName" or "groupId/guid.ext"
+                    // If you stored it that way, parse it out:
+                    // For example, if FileUrl is "https://.../group-messages/123/filename.jpg"
+                    // then the relative path is "123/filename.jpg"
+
+                    var containerUri = containerClient.Uri.ToString();
+                    // e.g. "https://anthra.blob.core.windows.net/group-messages"
+                    // We want to get what's after this containerUri + "/"
+
+                    var blobUrl = message.Attachment.FileUrl;
+                    if (blobUrl.StartsWith(containerUri))
+                    {
+                        var relativePath = blobUrl.Substring(containerUri.Length + 1);
+                        var blobClient = containerClient.GetBlobClient(relativePath);
+                        await blobClient.DeleteIfExistsAsync(DeleteSnapshotsOption.IncludeSnapshots);
+                    }
+
+                    // Remove from DB
+                    _context.Attachments.Remove(message.Attachment);
+                }
+                catch (Exception ex)
+                {
+                    // log the error if needed
+                }
+            }
+
+            _context.GroupMessages.Remove(message);
+            await _context.SaveChangesAsync();
+
+            // If you want to notify group members in real time:
+            // E.g., let them remove the message from their UI
+            var groupName = $"Group_{message.GroupId}";
+            await _hubContext.Clients.Group(groupName).SendAsync("GroupMessageDeleted", new
+            {
+                messageId = message.Id
+            });
+
+            return Ok(new { success = true, message = "Message deleted successfully." });
+        }
+    
+
+    public class DeleteGroupMessageRequest
+    {
+        public int MessageId { get; set; }
+    }
 
         /// <summary>
         /// Saves the file to Azure Blob Storage under the container "group-messages" and
@@ -324,5 +410,9 @@ public async Task<IActionResult> GetGroupChatHistory(int groupId, int pageSize =
                 throw; // or handle the error as you wish
             }
         }
-    }
+
+       
+}
+
+
 }

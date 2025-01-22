5c22cd4b53c556ce89ce19c33f6727ff4fe3139a
Tern Yuka Følbæk <tfolbaek@gmail.com>
2025-01-22 21:06:37 +0100

    Follow emerald scheme

--- AnthraClient/src/Dashboard/Helpers/Animations/NoConnections.tsx
+++ AnthraClient/src/Dashboard/Helpers/Animations/NoConnections.tsx
@@ -8,7 +8,7 @@ const NoConnectionsRive: React.FC = () => {
     return (
         <div className="no-connections-container">
             <p className="text-sm text-center font-medium text-gray-400 mb-2">No connections, explore new ones</p>
-            <button className="text-sm dark:bg-emerald-500 dark:hover:bg-emerald-400 explore-button" onClick={() => navigate('/dashboard/explore')}> <FaMagnifyingGlass/>Explore</button>
+            <button className="text-sm dark:bg-emerald-500 bg-emerald-400 hover:bg-emerald-300 dark:hover:bg-emerald-400 explore-button" onClick={() => navigate('/dashboard/explore')}> <FaMagnifyingGlass/>Explore</button>
         </div>
     )
 }

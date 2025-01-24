32c80db6f84b170181e03deed8e46d5ddb07a790
Tern Yuka Følbæk <tfolbaek@gmail.com>
2025-01-24 16:59:26 +0100

    Remove console log

--- AnthraClient/src/Dashboard/Helpers/Animations/NoMoreGroupsToExplore.tsx
+++ AnthraClient/src/Dashboard/Helpers/Animations/NoMoreGroupsToExplore.tsx
@@ -1,13 +1,10 @@
-import React, {useEffect} from "react";
+import React from "react";
 
 interface NoMoreGroupsToExploreProps {
     message?: string | null;
 }
 
 const NoMoreGroupsToExplore: React.FC<NoMoreGroupsToExploreProps> = ({ message }) => {
-    useEffect(() => {
-        console.log(message)
-    }, [message]);
     return (
         <div className="no-more-users-or-groups">
             <p className="dark:text-white text-m font-bold text-gray-500 text-center mb-2 ">

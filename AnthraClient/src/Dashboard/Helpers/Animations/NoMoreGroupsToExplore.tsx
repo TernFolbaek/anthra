a50860fc2a33b4e002cbd5776c25beeba89e93d2
Tern Yuka Følbæk <tfolbaek@gmail.com>
2025-01-24 16:48:50 +0100

    Display countdown message

--- AnthraClient/src/Dashboard/Helpers/Animations/NoMoreGroupsToExplore.tsx
+++ AnthraClient/src/Dashboard/Helpers/Animations/NoMoreGroupsToExplore.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useEffect} from "react";
 
-const NoMoreGroupsToExplore: React.FC = () => {
+interface NoMoreGroupsToExploreProps {
+    message?: string | null;
+}
 
+const NoMoreGroupsToExplore: React.FC<NoMoreGroupsToExploreProps> = ({ message }) => {
+    useEffect(() => {
+        console.log(message)
+    }, [message]);
     return (
         <div className="no-more-users-or-groups">
-            <p className="dark:text-white text-m font-bold text-gray-500 text-center mb-2 ">Awaiting new groups</p>
+            <p className="dark:text-white text-m font-bold text-gray-500 text-center mb-2 ">
+                {message ?? "Awaiting new groups"}
+            </p>
         </div>
-    )
-}
+    );
+};
 
 export default NoMoreGroupsToExplore;
--- AnthraClient/src/Dashboard/Helpers/Animations/NoMoreUsersToExplore.tsx
+++ AnthraClient/src/Dashboard/Helpers/Animations/NoMoreUsersToExplore.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 
-const NoMoreUsersToExplore: React.FC = () => {
+interface NoMoreUsersToExploreProps {
+    message?: string | null;
+}
 
+const NoMoreUsersToExplore: React.FC<NoMoreUsersToExploreProps> = ({ message }) => {
     return (
         <div className="no-more-users-or-groups">
-                <p className="dark:text-white text-m font-bold text-gray-500 text-center mb-2 ">Awaiting new users to join</p>
+            <p className="dark:text-white text-m font-bold text-gray-500 text-center mb-2 ">
+                {message ?? "Awaiting new users to join"}
+            </p>
         </div>
-    )
-}
+    );
+};
 
 export default NoMoreUsersToExplore;

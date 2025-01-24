2bf9cd155c981254f26bda8d6e2414aba9a7a6db
Tern Yuka Følbæk <tfolbaek@gmail.com>
2025-01-24 19:06:10 +0100

    Remove hover to not ruin UI on groups message deletion

--- AnthraClient/src/Dashboard/Helpers/Dialogs/ConfirmationDialog/ConfirmationDialog.tsx
+++ AnthraClient/src/Dashboard/Helpers/Dialogs/ConfirmationDialog/ConfirmationDialog.tsx
@@ -13,8 +13,8 @@ const ConfirmationDialog: React.FC<ConfirmationDialogProps> = ({ message, onConf
             <div className="confirmation-dialog dark-background-item-color">
                 <p className="text-base font-medium dark:text-white ">{message}</p>
                 <div className="confirmation-dialog-buttons justify-center flex gap-2">
-                    <button onClick={onConfirm} className="text-white hover:bg-emerald-300 px-4 py-2 rounded-md text-sm font-medium bg-emerald-400">Confirm</button>
-                    <button onClick={onCancel} className="text-white hover:bg-gray-500 px-4 py-2 rounded-md text-sm font-medium bg-gray-400">Cancel</button>
+                    <button onClick={onConfirm} className="text-white px-4 py-2  rounded-md text-sm font-medium bg-emerald-400">Confirm</button>
+                    <button onClick={onCancel} className="text-white px-4 py-2  rounded-md text-sm font-medium bg-gray-400">Cancel</button>
                 </div>
             </div>
         </div>

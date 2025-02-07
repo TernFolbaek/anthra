33c21787a0af32d3a9572a5165bef6254ad0664f
Tern Yuka Følbæk <tfolbaek@gmail.com>
2025-02-07 16:40:46 +0100

    Update profile view model to include stage of life and self studying subjects

--- AnthraServer/ViewModels/UpdateProfileViewModel.cs
+++ AnthraServer/ViewModels/UpdateProfileViewModel.cs
@@ -1,5 +1,6 @@
 // ViewModels/UpdateProfileViewModel.cs
 using System.Collections.Generic;
+using MyBackendApp.Models;
 
 namespace MyBackendApp.ViewModels
 {
@@ -10,7 +11,7 @@ namespace MyBackendApp.ViewModels
         public string? Location { get; set; }
         public string? Institution { get; set; }
         public string? Work { get; set; }
-        public string Courses { get; set; } // JSON string of courses
+        public string? Courses { get; set; } // JSON string of courses
         public List<string>? Subjects { get; set; }
         
         public List<string>? Statuses { get; set; }
@@ -18,6 +19,8 @@ namespace MyBackendApp.ViewModels
         public string? AboutMe { get; set; }
         public string? AllowEmailUpdates { get; set; }  
         public bool? IsProfileVisible { get; set; } 
+        public StageOfLife StageOfLife { get; set; }
+        public string? SelfStudyingSubjects { get; set; }
 
         public int? Age { get; set; }
         public IFormFile? ProfilePicture { get; set; }

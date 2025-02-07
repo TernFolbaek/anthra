b138d2029c684f6633b5264f51ee951bb075ee9a
Tern Yuka Følbæk <tfolbaek@gmail.com>
2025-02-07 16:40:51 +0100

    Migrate

---
+++ AnthraServer/Migrations/20250206144804_AddStageOfLife.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnthraBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStageOfLife : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<List<string>>(
+                name: "SelfStudyingSubjects",
+                table: "AspNetUsers",
+                type: "text[]",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StageOfLife",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SelfStudyingSubjects",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "StageOfLife",
+                table: "AspNetUsers");
+        }
+    }
+}
